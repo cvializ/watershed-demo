b2563dc19157b14290fcbec2b131366378be5057
Carlos Vializ <cvializ@vt.edu>
2026-07-06 22:25:15 -0400

    tests pass

--- tests/test-water-sources-system.ts
+++ tests/test-water-sources-system.ts
@@ -1,9 +1,10 @@
-import { expect } from 'chai';
+// import { expect } from 'chai';
 import * as THREE from 'three';
 import { GPUComputationRenderer } from 'three/addons/misc/GPUComputationRenderer.js';
 import './test-global-types.d.ts';
 
 import { createWaterSourcesSystem } from '../src/systems/createWaterSourcesSystem';
+import { test } from './testUtils.ts';
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -27,7 +28,7 @@ if (error !== null) {
 gpuCompute.compute();
 
 const resultTexture = gpuCompute.getCurrentRenderTarget(waterSourcesVariable).texture
- 
+
 const scene = new THREE.Scene();
 const geometry = new THREE.PlaneGeometry(1, 1);
 const material = new THREE.MeshPhongMaterial({
@@ -39,4 +40,10 @@ scene.add(mesh);
 const camera = new THREE.OrthographicCamera();
 renderer.render(scene, camera);
 
-expect(true).to.be.true;
+await test('addWater succeeds rendering at 0, 0', () => {
+    addWater(0, 0, 1, 1);
+});
+
+await test('clearWater succeeds', () => {
+    clearWater();
+});
