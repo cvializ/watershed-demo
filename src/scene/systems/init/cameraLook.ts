1af3bee5907f22a499b38a1b16e82eab0ee210a7
Carlos <carlos@example.com>
2026-07-28 22:03:51 -0400

    camera ref

--- src/scene/systems/init/cameraLook.ts
+++ src/scene/systems/init/cameraLook.ts
@@ -1,22 +1,36 @@
 import { observe, onAdd, query } from "bitecs";
+import * as THREE from "three";
 
 import type { SceneInitSystem } from "@/scene/types";
 
-import { Camera, MeshRef, Terrain } from "@/components/components";
+import {
+  MeshRef,
+  ObjectRef,
+  Renderable,
+  Terrain,
+} from "@/components/components";
+import { GeneralObjectEnum } from "@/scene/resources/generalObject";
 import { getMesh, MeshEnum } from "@/scene/resources/mesh";
-import { getCamera } from "@/scene/sceneUtils";
+import { getObject } from "@/scene/resources/objectCache";
 import { logger } from "@/utils/logger";
 
-export const cameraLookInitSystem: SceneInitSystem = (world, scene) => {
-  observe(world, onAdd(Camera), () => {
-    const camera = getCamera(scene);
-    if (!camera) {
-      logger.error("no camera in scene after add camera");
-      return;
+export const cameraLookInitSystem: SceneInitSystem = (world, _scene) => {
+  observe(world, onAdd(ObjectRef, Renderable), (entity$) => {
+    const objectRef = ObjectRef.ref[entity$];
+    if (!objectRef) {
+      throw new Error(`Entity ${entity$} ObjectRef not found`);
     }
+    const camera = getObject(objectRef as GeneralObjectEnum) as THREE.Camera;
 
     const [terrainEntity$] = query(world, [Terrain, MeshRef]);
+    if (!terrainEntity$) {
+      logger.error("no terrain found for camera lookAt");
+      return;
+    }
     const terrain = getMesh(MeshRef.ref[terrainEntity$] as MeshEnum);
+
     camera.lookAt(terrain.position);
+
+    logger.info("[camera:look-at] success");
   });
 };
