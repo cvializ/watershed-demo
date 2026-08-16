ec14e6924ee83f06e5b030481dc1268dbd504078
Carlos <carlos@example.com>
2026-08-15 22:02:51 -0400

    separate renderable init

---
+++ src/scene/systems/init/renderable.ts
@@ -0,0 +1,44 @@
+import { observe, onAdd, onRemove } from "bitecs";
+import * as THREE from "three";
+
+import type { SceneInitSystem } from "@/scene/types";
+
+import { MeshRef, ObjectRef, Renderable } from "@/components/components";
+import { getMesh, MeshEnum } from "@/scene/resources/mesh";
+import { GeneralObjectEnum } from "@/scene/resources/object";
+import { getObject } from "@/scene/resources/objectCache";
+import { logger } from "@/utils/logger";
+
+export const initRenderables: SceneInitSystem = (world, scene): void => {
+  logger.info("[renderable:init]");
+
+  // Handle MeshRef + Renderable entities
+  observe(world, onAdd(MeshRef, Renderable), (entity$) => {
+    logger.debug("RENDERABLE ADDED");
+    scene.add(getMesh(MeshRef.ref[entity$] as MeshEnum));
+  });
+
+  observe(world, onRemove(MeshRef, Renderable), (eid$) => {
+    logger.debug("RENDERABLE REMOVED");
+    logger.debug(`Remove mesh ${MeshRef.ref[eid$]}`);
+    scene.remove(getMesh(MeshRef.ref[eid$] as MeshEnum));
+  });
+
+  // Handle ObjectRef + Renderable entities
+  observe(world, onAdd(ObjectRef, Renderable), (entity$) => {
+    logger.debug("OBJECTREF RENDERABLE ADDED");
+    const objectRef = ObjectRef.ref[entity$];
+    if (objectRef) {
+      scene.add(getObject(objectRef as GeneralObjectEnum) as THREE.Object3D);
+    }
+  });
+
+  observe(world, onRemove(ObjectRef, Renderable), (eid$) => {
+    logger.debug("OBJECTREF RENDERABLE REMOVED");
+    const objectRef = ObjectRef.ref[eid$];
+    if (objectRef) {
+      logger.debug(`Remove object ${objectRef}`);
+      scene.remove(getObject(objectRef as GeneralObjectEnum));
+    }
+  });
+};
--- src/scene/systems/sceneInitSystem.ts
+++ src/scene/systems/sceneInitSystem.ts
@@ -1,52 +1,18 @@
-import { observe, onAdd, onRemove } from "bitecs";
-import * as THREE from "three";
-
 import type { SceneInitSystem } from "@/scene/types";
 
-import { MeshRef, ObjectRef, Renderable } from "@/components/components";
-import { getMesh, MeshEnum } from "@/scene/resources/mesh";
-import { GeneralObjectEnum } from "@/scene/resources/object";
-import { getObject, initObjects } from "@/scene/resources/objectCache";
+import { initObjects } from "@/scene/resources/objectCache";
 import { cameraLookInitSystem } from "@/scene/systems/init/cameraLook";
 import { hiddenInitSystem } from "@/scene/systems/init/hidden";
 import { initMaterials } from "@/scene/systems/init/material";
 import { initMeshes } from "@/scene/systems/init/mesh";
+import { initRenderables } from "@/scene/systems/init/renderable";
 import { initTextures } from "@/scene/systems/init/texture";
 import { logger } from "@/utils/logger";
 
 export const sceneInitSystem: SceneInitSystem = (world, scene): void => {
   logger.info("[scene:init]");
 
-  // Handle MeshRef + Renderable entities
-  observe(world, onAdd(MeshRef, Renderable), (entity$) => {
-    logger.debug("RENDERABLE ADDED");
-    scene.add(getMesh(MeshRef.ref[entity$] as MeshEnum));
-  });
-
-  observe(world, onRemove(MeshRef, Renderable), (eid$) => {
-    logger.debug("RENDERABLE REMOVED");
-    logger.debug(`Remove mesh ${MeshRef.ref[eid$]}`);
-    scene.remove(getMesh(MeshRef.ref[eid$] as MeshEnum));
-  });
-
-  // Handle ObjectRef + Renderable entities
-  observe(world, onAdd(ObjectRef, Renderable), (entity$) => {
-    logger.debug("OBJECTREF RENDERABLE ADDED");
-    const objectRef = ObjectRef.ref[entity$];
-    if (objectRef) {
-      scene.add(getObject(objectRef as GeneralObjectEnum) as THREE.Object3D);
-    }
-  });
-
-  observe(world, onRemove(ObjectRef, Renderable), (eid$) => {
-    logger.debug("OBJECTREF RENDERABLE REMOVED");
-    const objectRef = ObjectRef.ref[eid$];
-    if (objectRef) {
-      logger.debug(`Remove object ${objectRef}`);
-      scene.remove(getObject(objectRef as GeneralObjectEnum));
-    }
-  });
-
+  initRenderables(world, scene);
   initTextures(world, scene);
   initMaterials(world, scene);
   initMeshes(world, scene);
